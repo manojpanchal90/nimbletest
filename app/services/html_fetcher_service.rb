5eb3f3e1ab5bb9cc3c864c8416137bd37b9c650b
manojpanchal90 <manoj.panchal@kiwitech.com>
2023-11-24 19:17:53 +0530

    Add the Upload Feature

--- app/services/html_fetcher_service.rb
+++ app/services/html_fetcher_service.rb
@@ -3,14 +3,21 @@ require 'nokogiri'
 require 'open-uri'
 
 class HtmlFetcherService
-  def initialize(url)
-    @url = url
+  attr_accessor :search_url, :keyword
+
+  def initialize(search_url, keyword)
+    @search_url  = search_url
+    @keyword = keyword
   end
 
   def fetch_and_store_information
     html_content = fetch_html_content
-    links = extract_links(html_content)
-
+    doc = Nokogiri::HTML(html_content)
+    #search_count = extract_search_count(doc)
+    links        = extract_links(doc).size
+    #binding.pry
+    #search_count = extract_search_count(doc)
+    #binding.pry
     save_information(html_content, links)
   end
 
@@ -18,7 +25,7 @@ class HtmlFetcherService
 
   def fetch_html_content
     begin
-      response = HTTParty.get(@url)
+      response = HTTParty.get(search_url)
       response.body
     rescue StandardError => e
       # Handle error, e.g., log it or raise a custom exception
@@ -26,15 +33,28 @@ class HtmlFetcherService
     end
   end
 
-  def extract_links(html_content)
-    doc = Nokogiri::HTML(html_content)
+  def extract_links(doc)
+    #doc = Nokogiri::HTML(html_content)
     doc.css('a').map { |link| link['href'] }
   end
 
+  def extract_search_count(doc)
+    result_stats_text = doc.css('#result-stats').text.strip
+    #binding.pry
+    result_number = result_stats_text.match(/(\d+)/)[1].to_i
+  end
+
   def save_information(html_content, links)
+    begin
+      user = Keyword.find_or_create_by!(name: keyword) do |u|
+        u.total_links = links
+        u.html_content =  html_content
+      end
+    rescue ActiveRecord::RecordInvalid => e
+      puts "Error: #{e.message}"
+    end
     # Save the HTML content and links to your database or wherever you prefer
     # Example: You can create a model/table to store this information
     # Information.create(url: @url, html_content: html_content, links: links)
   end
 end
-Use the service i
--- app/workers/process_keyword_worker.rb
+++ app/workers/process_keyword_worker.rb
@@ -1,9 +1,13 @@
 class ProcessKeywordWorker
   include Sidekiq::Worker
 
-  def perform()
+  def perform(keyword, search_site)
     # Perform your individual operation on each row here
     # For example, you can create a Keyword record
-    url = params[:url]
-    HtmlFetcherService.new(url).fetch_and_store_information
+    HtmlFetcherService.new("#{search_site}?search=keyword", keyword).fetch_and_store_information
+  end
+
+  # def search_url
+  #   "#{search_site}?search=keyword"
+  # end
 end
