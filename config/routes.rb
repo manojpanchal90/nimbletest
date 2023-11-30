426a70968157e061d70686e29fb71143b92317f7
manoj <manojpanchal90@gmail.com>
2023-12-01 00:49:07 +0530

    Fix: Render the stored HTML in the UI (#6)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  resources :keywords
+  resources :keywords  do
+
+    get 'scrap_html', on: :member
+  end
+
+  resources :jobs, only: [:create]
+
+
   get 'home/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
